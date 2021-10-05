fead5e697f3a1e8c5cc5d8d636d0ded3760ce939
Jose Ángel Rodríguez <joseangelrodrrodr@gmail.com>
2021-10-05 09:21:46 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Add/add_floats.h"
+int main(int argc, char *argv[]){
+    std::cout<<"Something to output for example... Dunno."<<std::endl;
+    std::cout<<add_float(3.2f,4.5f)<<std::endl;
+    return 0;
+}
