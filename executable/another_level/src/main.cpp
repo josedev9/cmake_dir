8e3422566a760a406e099604b719162ce5501529
Jose <joseangelrodrrodr@gmail.com>
2021-10-12 17:54:20 +0200

    Added the configuration for submodules in windows, hopefully this works now

--- executable/another_level/src/main.cpp
+++ executable/another_level/src/main.cpp
@@ -11,8 +11,9 @@
 #endif
 
 int main(int argc, char const * argv[]){
-    std::cout<<"The file called '"<<argv[0]<<"' has the following versions: "<<windows_VERSION_MAJOR<<" and "<<windows__VERSION_MINOR<<std::endl;
+    std::cout<<"The file called '"<<argv[0]<<"' has the following versions: "<<windows_VERSION_MAJOR<<" and "<<windows_VERSION_MINOR<<std::endl;
+    File *ptr=new File("Test object",300);
+    ptr->get_num();
 
-    
     return 0;
 }
